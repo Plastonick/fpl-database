17dbdd2cef8cb4ba224b615286e674ade2b152b7
David Pugh <dpugh@podfather.com>
2021-12-16 21:04:25 +0000

    add migrations and testing postgres db

--- db/migrations/20211216201259_create_fantasy_tables.php
+++ db/migrations/20211216201259_create_fantasy_tables.php
@@ -1,23 +1,15 @@
 <?php
 declare(strict_types=1);
 
+use Phinx\Db\Adapter\PostgresAdapter;
 use Phinx\Migration\AbstractMigration;
 
 final class CreateFantasyTables extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change(): void
     {
-
+        $this->table('players')
+            ->addColumn('player_id', PostgresAdapter::PHINX_TYPE_BIG_INTEGER)
+            ->save();
     }
 }
