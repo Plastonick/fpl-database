7aa20bdd7a058d09e502da3d14adafe5e788c6c5
David Pugh <plastonick@gmail.com>
2022-01-03 18:33:01 +0000

    improve the build process

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# check if there's a Fantasy-Premier-League dir
+[ ! -d './Fantasy-Premier-League'] && git clone https://github.com/vaastav/Fantasy-Premier-League.git Fantasy-Premier-League
+
+cd Fantasy-Premier-League && git checkout master && git pull
+
 docker-compose down
 docker-compose up -d
 docker-compose exec hydration composer install
@@ -7,6 +12,7 @@ sleep 10 # give postgres enough time to start up
 docker-compose exec hydration vendor/bin/phinx migrate
 docker-compose exec hydration php hydrate.php
 
-# snapshot psql image
+CONTAINER_ID=`docker-compose ps -q database`
+docker commit $CONTAINER_ID david-pugh/fpl-database:latest
 
-#docker-compose down
+docker-compose down
