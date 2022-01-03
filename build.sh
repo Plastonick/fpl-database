fd967f83abcd368db55541905ece99240d4aec81
David Pugh <plastonick@gmail.com>
2022-01-03 19:15:12 +0000

    extract repository

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+$REPOSITORY=davidpugh/fpl-database
+
 # check if there's a Fantasy-Premier-League dir
 FANTASY_DIR=`pwd`/Fantasy-Premier-League
 if [ ! -d "$FANTASY_DIR" ]; then
@@ -15,7 +17,11 @@ sleep 10 # give postgres enough time to start up
 docker compose exec hydration vendor/bin/phinx migrate
 docker compose exec hydration php hydrate.php
 
+DATE_STRING=`date +"%F"`
+
 CONTAINER_ID=`docker compose ps -q database`
-docker commit $CONTAINER_ID david-pugh/fpl-database:latest
+docker commit $CONTAINER_ID $REPOSITORY:$DATE_STRING
+
+docker tag $REPOSITORY:$DATE_STRING $REPOSITORY:latest
 
 docker compose down
