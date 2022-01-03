74c67f96507552e8969c0dcfdcdc2dadcf6aede0
David Pugh <plastonick@gmail.com>
2022-01-03 19:07:02 +0000

    use docker compose for better (easier...) compatibility with ubuntu fix the bash if condition... damned whitespace

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # check if there's a Fantasy-Premier-League dir
-[ ! -d './Fantasy-Premier-League'] && git clone https://github.com/vaastav/Fantasy-Premier-League.git Fantasy-Premier-League
+FANTASY_DIR=`pwd`/Fantasy-Premier-League
+if [ ! -d "$FANTASY_DIR" ]; then
+  git clone https://github.com/vaastav/Fantasy-Premier-League.git Fantasy-Premier-League
+fi
 
 cd Fantasy-Premier-League && git checkout master && git pull
 
-docker-compose down
-docker-compose up -d
-docker-compose exec hydration composer install
+docker compose down
+docker compose up -d
+docker compose exec hydration composer install
 sleep 10 # give postgres enough time to start up
-docker-compose exec hydration vendor/bin/phinx migrate
-docker-compose exec hydration php hydrate.php
+docker compose exec hydration vendor/bin/phinx migrate
+docker compose exec hydration php hydrate.php
 
-CONTAINER_ID=`docker-compose ps -q database`
+CONTAINER_ID=`docker compose ps -q database`
 docker commit $CONTAINER_ID david-pugh/fpl-database:latest
 
-docker-compose down
+docker compose down
