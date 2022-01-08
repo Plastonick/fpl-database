dc22b00abb869e8ab0e4fe30d804e6bd422317f9
David Pugh <plastonick@gmail.com>
2022-01-08 14:57:08 +0000

    add global hydration

--- hydrate.php
+++ hydrate.php
@@ -3,20 +3,16 @@
 use Plastonick\FantasyDatabase\Hydration\FixturesHydration;
 use Plastonick\FantasyDatabase\Hydration\GameWeekHydration;
 use Plastonick\FantasyDatabase\Hydration\PlayerPeformanceHydration;
-use Plastonick\FantasyDatabase\Hydration\SeasonsHydration;
-use Plastonick\FantasyDatabase\Hydration\TeamsHydration;
+use Plastonick\FantasyDatabase\Hydration\GlobalHydration;
 
 include 'vendor/autoload.php';
 
-$logger = new Monolog\Logger('stdout', [new Monolog\Handler\StreamHandler('php://stdout')]);
+$logger = new Monolog\Logger('hydration', [new Monolog\Handler\StreamHandler('php://stdout')]);
 
 $connection = new \PDO("pgsql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}", $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
-$seasons = new SeasonsHydration($connection, $logger);
-$seasons->hydrate(__DIR__ . '/Fantasy-Premier-League/data/');
-
-$teams = new TeamsHydration($connection, $logger);
-$teams->hydrate(__DIR__ . '/Fantasy-Premier-League/data/');
+$global = new GlobalHydration($connection, $logger);
+$global->hydrate(__DIR__ . '/Fantasy-Premier-League/data/');
 
 $gameWeek = new GameWeekHydration($connection, $logger);
 $gameWeek->hydrate(__DIR__ . '/Fantasy-Premier-League/data/');
